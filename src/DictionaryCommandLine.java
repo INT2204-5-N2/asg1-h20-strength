179d6419f564f46b17f75c15db40d1506a986bcd
manhmin119 <43264991+manhmin119@users.noreply.github.com>
2018-10-30 22:13:06 +0700

    Update DictionaryCommandLine.java

--- src/DictionaryCommandLine.java
+++ src/DictionaryCommandLine.java
@@ -5,20 +5,14 @@ import java.util.Scanner;
 
 public class DictionaryCommandLine{
     private static DictionaryManagement dictionaryManagement = new  DictionaryManagement();
- /**
-  * Hàm hiển thị tất cả cá word của từ điển
-  * @param dictionary 
-  */
+ 
     public static void showAllWords(Dictionary dictionary){
         List<Word> ws = dictionary.getWords();
         System.out.println("No\t |English \t |Vietnamese");
         for(int i=0; i < ws.size(); i++){
             System.out.println( (i+1) +"\t |"+ ws.get(i).getWord_target() + "\t |" +ws.get(i).getWord_explain());
         }
-//        for(Word w : ws)
-//        {
-//            System.out.println( "\t "+ w.getWord_target() +"\t "+w.getWord_explain());
-//        }
+
     }
     
     public static void dictionaryBasic() {
