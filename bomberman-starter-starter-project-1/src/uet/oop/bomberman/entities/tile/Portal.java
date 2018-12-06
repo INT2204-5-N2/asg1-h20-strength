9f7346e3e85564fcff5e6f1f935ffaacbb82a0b8
mạnh <nguyenducmanh2581912@gmail.com>
2018-12-06 22:19:56 +0700

    ok
    v

--- bomberman-starter-starter-project-1/src/uet/oop/bomberman/entities/tile/Portal.java
+++ bomberman-starter-starter-project-1/src/uet/oop/bomberman/entities/tile/Portal.java
@@ -1,6 +1,7 @@
 package uet.oop.bomberman.entities.tile;
 
 import uet.oop.bomberman.entities.Entity;
+import uet.oop.bomberman.entities.character.Bomber;
 import uet.oop.bomberman.graphics.Sprite;
 
 public class Portal extends Tile {
@@ -12,6 +13,18 @@ public class Portal extends Tile {
 	@Override
 	public boolean collide(Entity e) {
 		// TODO: xử lý khi Bomber đi vào
+                if(e instanceof Bomber ) {			
+			if(((Bomber) e).getBoard().detectNoEnemies() == false)
+				return false;			
+			if(e.getXTile() == getX() && e.getYTile() == getY()) {
+				if(((Bomber) e).getBoard().detectNoEnemies())
+					((Bomber) e).getBoard().nextLevel();
+                               
+			}
+			
+			return true;
+		}
+
 		return false;
 	}
 
